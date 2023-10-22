196895cc03703196a9531ce9f451708e3b422cfb
Hussein <social.101.acc@gmail.com>
2023-10-22 20:38:29 +0300

    added dependecny for the messageoperation for specifying the destination and payload of message that are captured by the listener

--- src/main/java/com/hf/chat_application/config/WebSocketEventListener.java
+++ src/main/java/com/hf/chat_application/config/WebSocketEventListener.java
@@ -1,19 +1,35 @@
 package com.hf.chat_application.config;
 
+import com.hf.chat_application.chat.ChatMessage;
+import com.hf.chat_application.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
-import java.util.EventListener;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageSendingOperations;
+
+    @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
 
+        StompHeaderAccessor headerAccessor= StompHeaderAccessor.wrap(event.getMessage());
+        String username =(String) headerAccessor.getSessionAttributes().get("username");
+        if(username!=null){
+            log.info("User Disconnected: {}",username);
+            var chatmessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageSendingOperations.convertAndSend("/topic/public",chatmessage);
+        }
 
     }
 }
